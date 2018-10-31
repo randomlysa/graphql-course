3532a92c58c0ac1bfc6ae9e74b674e4424cdd463
randomlysa <su@randomlysa.com>
2018-10-31 16:14:28 -0500

    remove info about db
    no longer using db and this functionality is included in prisma. keeping this code blocks relational queries from working

--- graphql-prisma/src/resolvers/Comment.js
+++ graphql-prisma/src/resolvers/Comment.js
@@ -1,12 +1,5 @@
 const Comment = {
-  author(parent, args, { db }, info) {
-    return db.users.find(user => {
-      return parent.author ===  user.id
-    })
-  },
-  post(parent, args, { db }, info) {
-    return db.posts.find(post => parent.post === post.id)
-  }
+
 }
 
 export { Comment as default }
