c83c779118eac18d686bd808ee9c0d2963810b6d
randomlysa <su@randomlysa.com>
2018-11-01 12:14:18 -0500

    update updateUser to authorize with header/token

--- graphql-prisma/src/resolvers/Mutation.js
+++ graphql-prisma/src/resolvers/Mutation.js
@@ -42,10 +42,12 @@ const Mutation = {
     }, info)
   }, // deleteUser
 
-  async updateUser(parent, args, { prisma }, info) {
+  async updateUser(parent, args, { prisma, request }, info) {
+    const userId = getUserId(request)
+
     return prisma.mutation.updateUser({
       where: {
-        id: args.id
+        id: userId
       },
       data: args.data
     }, info)
