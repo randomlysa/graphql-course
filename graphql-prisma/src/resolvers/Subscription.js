78ac01673708c161353b585c6c58d55ec92e0a90
randomlysa <su@randomlysa.com>
2018-10-31 20:59:42 -0500

    subscribe to comments using prisma

--- graphql-prisma/src/resolvers/Subscription.js
+++ graphql-prisma/src/resolvers/Subscription.js
@@ -1,11 +1,15 @@
 const Subscription = {
   comment: {
-    subscribe(parent, { postId }, { db, pubsub }, info) {
-      const post = db.posts.find(post => post.id === postId && post.published)
-
-      if (!post) throw new Error ('Post not found or not published')
-
-      return pubsub.asyncIterator(`comment ${postId}`)
+    subscribe(parent, { postId }, { prisma }, info) {
+      return prisma.subscription.comment({
+        where: {
+          node: {
+            post: {
+              id: postId
+            }
+          }
+        }
+      }, info)
     }
   },
   post: {
