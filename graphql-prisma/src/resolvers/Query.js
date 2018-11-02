fc4f3749a81c814ae54f99983d5c1129bf787fe2
randomlysa <su@randomlysa.com>
2018-11-02 10:56:29 -0500

    set up pagination for users, posts
    for users, add 'after' option

--- graphql-prisma/src/resolvers/Query.js
+++ graphql-prisma/src/resolvers/Query.js
@@ -5,7 +5,8 @@ const Query = {
     users(parent, args, { prisma }, info) {
       const opArgs = {
         first: args.first,
-        skip: args.skip
+        skip: args.skip,
+        after: args.after
       }
 
       if (args.query) {
@@ -23,6 +24,9 @@ const Query = {
     posts(parent, args, { prisma }, info) {
       // By default, show only published posts.
       const opArgs = {
+        first: args.first,
+        skip: args.skip,
+        after: args.after,
         where: {
           published: true
         }
