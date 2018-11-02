4d34f737894f76b5099e623d28a30d048bd62b12
randomlysa <su@randomlysa.com>
2018-11-01 20:40:26 -0500

    hide all email addresses except user can see own

--- graphql-prisma/src/resolvers/User.js
+++ graphql-prisma/src/resolvers/User.js
@@ -1,5 +1,19 @@
-const User = {
+import getUserId from '../utils/getUserId'
 
+const User = {
+  email(parent, args, { request }, info) {
+    const userId = getUserId(request, false)
+
+    // Return an email only for the logged in user.
+    // Note the query must return/select user id
+    // otherwise all emails will be hidden.
+    if (userId && userId === parent.id) {
+      return parent.email
+    }
+    else {
+      return null
+    }
+  }
 }
 
 export { User as default }
