66f65a86bb46e9ccc411b338dba0ff44b2546bdd
randomlysa <su@randomlysa.com>
2018-11-05 17:12:04 -0600

    bug fix - check password.length not password

--- graphql-prisma/src/utils/hashPassword.js
+++ graphql-prisma/src/utils/hashPassword.js
@@ -2,7 +2,7 @@ import bcrypt from 'bcryptjs'
 
 const hashPassword = (password) => {
   // Check if password is 8 characters or longer.
-  if(password < 8) {
+  if(password.length < 8) {
     throw new Error('Password must be 8 characters or more.')
   }
 
