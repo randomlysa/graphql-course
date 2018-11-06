a7dbc991d072767b703823985b83d395e714f334
randomlysa <su@randomlysa.com>
2018-11-06 15:44:24 -0600

    check that myPosts returns 2 posts (published and not)

--- graphql-prisma/tests/post.test.js
+++ graphql-prisma/tests/post.test.js
@@ -1,6 +1,6 @@
 import 'cross-fetch/polyfill'
 import { gql } from 'apollo-boost'
-import seedDatabase from './utils/seedDatabase'
+import seedDatabase, { userOne } from './utils/seedDatabase'
 import getClient from './utils/getClient'
 
 const client = getClient()
@@ -20,3 +20,20 @@ test('Should show only published posts', async () => {
   expect(response.data.posts.length).toBe(1)
   expect(response.data.posts[0].published).toBe(true)
 }) // Should show only published posts
+
+
+test('Get myPosts (published and not)', async () => {
+  const client = getClient(userOne.jwt)
+  const myPosts = gql`
+    query {
+      myPosts {
+        id
+        title
+        published
+      }
+    }
+  `
+  const { data } = await client.query({ query: myPosts })
+
+  expect(data.myPosts.length).toBe(2)
+})
