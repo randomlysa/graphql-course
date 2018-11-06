2153f03a9be10990b6376fec2fbe0d6b240d7bd9
randomlysa <su@randomlysa.com>
2018-11-06 15:32:56 -0600

    use a JWT to test getting a profile

--- graphql-prisma/tests/utils/getClient.js
+++ graphql-prisma/tests/utils/getClient.js
@@ -1,8 +1,17 @@
 import ApolloBoost from 'apollo-boost'
 
-const getClient =  () => {
+const getClient = (jwt) => {
   return new ApolloBoost({
-    uri: 'http://localhost:4001'
+    uri: 'http://localhost:4001',
+    request(operation) {
+      if (jwt) {
+        operation.setContext({
+          headers: {
+            Authorization: `Bearer ${jwt}`
+          }
+        })
+      }
+    }
   })
 }
 
--- graphql-prisma/tests/utils/seedDatabase.js
+++ graphql-prisma/tests/utils/seedDatabase.js
@@ -1,17 +1,28 @@
 import bcrypt from 'bcryptjs'
+import jwt from 'jsonwebtoken'
 import prisma from '../../src/prisma';
 
+const userOne = {
+  input: {
+    name: 'User1',
+    email: 'user1@example.com',
+    password: bcrypt.hashSync('zxc098!@#')
+  },
+  user: undefined,
+  jwt: undefined
+}
+
 const seedDatabase = async () => {
+  // Delete test data
   await prisma.mutation.deleteManyPosts()
   await prisma.mutation.deleteManyUsers()
-  const user = await  prisma.mutation.createUser({
-    data: {
-      name: 'User1',
-      email: 'user1@example.com',
-      password: bcrypt.hashSync('zxc098!@#')
 
-    }
+  // Create userOne
+  userOne.user = await prisma.mutation.createUser({
+    data: userOne.input
   })
+  userOne.jwt = jwt.sign({ userId: userOne.user.id }, process.env.JWT_SECRET)
+
   await prisma.mutation.createPost({
     data: {
       title: 'Exmaple Post 1 (Published)',
@@ -19,7 +30,7 @@ const seedDatabase = async () => {
       body: '',
       author: {
         connect: {
-          id: user.id
+          id: userOne.user.id
         }
       }
     }
@@ -31,11 +42,11 @@ const seedDatabase = async () => {
       body: '',
       author: {
         connect: {
-          id: user.id
+          id: userOne.user.id
         }
       }
     }
   })
 }
 
-export { seedDatabase as default }
+export { seedDatabase as default, userOne }
