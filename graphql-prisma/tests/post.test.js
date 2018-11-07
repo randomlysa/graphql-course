73ff5d4d1daf61906128810d80dfa4e6940dcab9
randomlysa <su@randomlysa.com>
2018-11-06 18:44:49 -0600

    test creating, deleting a post

--- graphql-prisma/tests/post.test.js
+++ graphql-prisma/tests/post.test.js
@@ -1,7 +1,7 @@
 import 'cross-fetch/polyfill'
 import { gql } from 'apollo-boost'
 import prisma from '../src/prisma'
-import seedDatabase, { userOne, postOne } from './utils/seedDatabase'
+import seedDatabase, { userOne, postOne, postTwo } from './utils/seedDatabase'
 import getClient from './utils/getClient'
 
 const client = getClient()
@@ -65,4 +65,46 @@ test('Should be able to update own post', async () => {
   expect(data.updatePost.published).toBe(false)
   expect(exists).toBe(true)
 
-})
+test('Should be able to create a post', async () => {
+  const client = getClient(userOne.jwt)
+  const createPost = gql`
+    mutation {
+      createPost (
+        data: {
+          title: "A test post",
+          body: ""
+          published: true
+        }
+      ) {
+          id
+          title
+          body
+          published
+      }
+    }
+  `
+  const { data } = await client.mutate({ mutation: createPost })
+  const exists = await prisma.exists.Post({ id: data.createPost.id })
+
+  expect(data.createPost.title).toBe('A test post')
+  expect(data.createPost.published).toBe(true)
+  expect(exists).toBe(true)
+}) // Should be able to create a post
+
+test('Should be able to delete a post', async () => {
+  const client = getClient(userOne.jwt)
+  const deletePost = gql`
+    mutation {
+      deletePost (
+        id: "${postTwo.post.id}"
+      ) {
+          id
+      }
+    }
+  `
+
+  await client.mutate({ mutation: deletePost })
+  const exists = await prisma.exists.Post({ id: postTwo.post.id })
+
+  expect(exists).toBe(false)
+}) // Should be able to delete a post
