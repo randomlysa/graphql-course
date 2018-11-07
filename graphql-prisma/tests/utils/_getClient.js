d7073baa38ec9fd3170148ebbce1b8c8cf385c2b
randomlysa <su@randomlysa.com>
2018-11-07 13:17:02 -0600

    replace getClient with a version that supports subscriptions

---
+++ graphql-prisma/tests/utils/_getClient.js
@@ -0,0 +1,19 @@
+// Original getClient file that doesn't support subscriptions
+import ApolloBoost from 'apollo-boost'
+
+const getClient = (jwt) => {
+  return new ApolloBoost({
+    uri: 'http://localhost:4001',
+    request(operation) {
+      if (jwt) {
+        operation.setContext({
+          headers: {
+            Authorization: `Bearer ${jwt}`
+          }
+        })
+      }
+    }
+  })
+}
+
+export { getClient as default }
