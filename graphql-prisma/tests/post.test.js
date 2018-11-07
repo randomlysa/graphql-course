fe6619392cfac420c99299abd9b568aab03371d1
randomlysa <su@randomlysa.com>
2018-11-06 18:16:35 -0600

    test updating own post

--- graphql-prisma/tests/post.test.js
+++ graphql-prisma/tests/post.test.js
@@ -1,6 +1,7 @@
 import 'cross-fetch/polyfill'
 import { gql } from 'apollo-boost'
-import seedDatabase, { userOne } from './utils/seedDatabase'
+import prisma from '../src/prisma'
+import seedDatabase, { userOne, postOne } from './utils/seedDatabase'
 import getClient from './utils/getClient'
 
 const client = getClient()
@@ -21,8 +22,7 @@ test('Should show only published posts', async () => {
   expect(response.data.posts[0].published).toBe(true)
 }) // Should show only published posts
 
-
-test('Get myPosts (published and not)', async () => {
+test('Should get myPosts (published and not)', async () => {
   const client = getClient(userOne.jwt)
   const myPosts = gql`
     query {
@@ -36,4 +36,33 @@ test('Get myPosts (published and not)', async () => {
   const { data } = await client.query({ query: myPosts })
 
   expect(data.myPosts.length).toBe(2)
+}) // Get myPosts (published and not)
+
+test('Should be able to update own post', async () => {
+  const client = getClient(userOne.jwt)
+  const updatePost = gql`
+    mutation {
+      updatePost (
+        id: "${postOne.post.id}",
+        data: {
+          published: false
+        }
+      ) {
+          id
+          title
+          body
+          published
+      }
+    }
+  `
+
+  const { data } = await client.mutate({ mutation: updatePost })
+  const exists = await prisma.exists.Post({
+    id: postOne.post.id,
+    published: false
+  })
+
+  expect(data.updatePost.published).toBe(false)
+  expect(exists).toBe(true)
+
 })
